4df2ccf424490bf4154478960c6a317152346a40
Gabriela M. Fernández <galalou2010@gmail.com>
2023-03-29 11:33:07 +0200

    first group migration ready

---
+++ database/migrations/2023_03_29_093045_create_promotions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('trainer');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->foreignId('topic_id')->constrained('topics');
+            $table->string('evaluation1')->nullable();
+            $table->string('evaluation2')->nullable();
+            $table->string('evaluation3')->nullable();
+            $table->string('evaluation4')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotions');
+    }
+};
--- database/migrations/2023_03_29_093137_create_users_table.php
+++ database/migrations/2023_03_29_093137_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('rol', ['admin', 'coder', 'trainer']);
+            $table->foreignId('promotion_id')->constrained('promotions');
             $table->timestamps();
         });
     }
