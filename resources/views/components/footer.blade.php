c7b82b91e0fb3334293bfc4ee9b3a0a64da533c6
Marjane Oliveira <marjane_rodrigues@hotmail.com>
2023-04-05 10:37:56 +0200

    modificado footer y navegation

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="bottom-0 left-0 z-20 w-full bg-black text-white p-4 text-sm flex justify-center items-center clear-both fixed inset-x-0 bottom-0 w-full">
+<footer class="bg-black text-white p-6">
     <p class="text-center">©Copyright 2023 Factoría F5</p>
 </footer>
