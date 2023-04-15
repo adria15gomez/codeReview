cc99e0c33ab6f9d3989e2939a2d6ad8559c3cf84
SharonInfante <sharonshabel@gmail.com>
2023-04-16 01:15:37 +0200

    added backend functionalities and minimum evaluation styles

--- resources/views/trainer/coders.blade.php
+++ resources/views/trainer/coders.blade.php
@@ -29,38 +29,37 @@
 <div class="md:w-6/12 p-6 ">
         <h3 class="font-medium text-2xl py-8 ml-10 md:flex md:justify-center">Lista de coders</h3>
 
-        <div class="max-w-sm md:flex md:align-center p-6 mx-5 border ml-10 mt-0.5 border-gray-200 rounded-lg shadow bg-[#111827]">
-            <div class="flex justify-between pt-4 mb-1">
-                <span class="text-lg font-medium text-white">Laura Garcia</span>
-                <span>
-                    <svg class="flex h-7 w-8 text-white" aria-hidden="true" focusable="false" data-prefix="fab"
-                        data-icon="arrow" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path
-                            fill="currentColor"d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z" />
-                    </svg>
-                </span>
+        @foreach ($users as $user)
+            <div class="text-white w-5/6 justify-around text-base my-10  ml-10 bg-[#111827] hover:bg-[#111827]/80 focus:ring-4 focus:outline-none focus:ring-[#111827]/50 rounded-lg text-md py-4 px-2  text-center items-center block">
+                <div class="flex justify-between pt-4 mb-1">
+                    <span class="text-lg font-medium text-white">{{$user->name}}</span>
+                    <span>
+                        <svg class="flex h-7 w-8 text-white" aria-hidden="true" focusable="false" data-prefix="fab"
+                            data-icon="arrow" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path
+                                fill="currentColor"d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z" />
+                        </svg>
+                    </span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full my-10">
+                    <div class="h-6 bg-orange-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                        style="width: 75%">75%</div>
+                </div>
+                <div class="flex justify-center">
+                    <a href="#" class="flex-row text-white mr-4 cursor-pointer">Ver</a>
+                    <span class="mx-4 text-white">|</span>
+                    <a href="" class=" flex-row text-white mr-4 cursor-pointer">Editar</a>
+                    <span class="mx-4 text-white">|</span>
+                    <form action="" method="POST">
+    
+                        @csrf
+                        @method('delete')
+    
+                        <button class="flex-row text-orange-600 cursor-pointer" type="submit">Eliminar</button>
+                    </form>  
+                </div> 
             </div>
-            <div class="w-full bg-gray-200 rounded-full my-10">
-                <div class="h-6 bg-orange-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-                    style="width: 75%">75%</div>
-            </div>
-            <div class="row-grid">
-                <input type="submit" value="Ver"class="text-white ml-10 cursor-pointer"></input>
-                <input type="submit" value="Editar"class="text-white mx-10 cursor-pointer"></input>
-                <input type="submit" value="Eliminar"class=" mr-4 text-orange-600 cursor-pointer"></input>
-            </div>
-        </div>
-
-        <button type="button"
-            class="text-white w-5/6 justify-around text-base my-10  ml-10 bg-[#111827] hover:bg-[#111827]/80 focus:ring-4 focus:outline-none focus:ring-[#111827]/50 rounded-lg text-md py-4 px-2  text-center inline-flex items-center">
-            Sergio González
-            <svg class="flex h-7 ml-4 -mr-1 w-8" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="arrow"
-                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path fill="currentColor"
-                    d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z" />
-            </svg>
-        </button>
-
+        @endforeach
 </div>
     </div>
 @endsection
--- resources/views/trainer/promotions.blade.php
+++ resources/views/trainer/promotions.blade.php
@@ -15,61 +15,31 @@
         </svg>
     </button>
 
-    <ul>
-        @foreach ($promotions as $promotion)
-            <li>
-                <strong>{{ $promotion->name }}</strong>
-                <br>
-                Formador principal: {{ $promotion->trainer }}
-                <br>
-                Fecha de inicio: {{ $promotion->start_date }}
-                <br>
-                Fecha de final:{{ $promotion->end_date }}
-                <br>
-                Enlace al Zoom:{{ $promotion->zoom_url }}
-                <br>
-                Enlace al Slack: {{ $promotion->slack_url }}
-                <br>
-                <a href="{{ route('editPromotion.edit', $promotion) }}"> Editar </a>
-                <form action="{{ route('deletePromotion.destroy', $promotion) }}" method="POST">
+    <h3 class="font-medium text-2xl  ml-10 ">Lista de bootcamps</h3>
+
+    @foreach ($promotions as $promotion)
+        <div class="max-w-sm p-6 mx-5 border ml-10 mt-4 border-gray-200 rounded-lg shadow bg-[#111827] flex flex-col">
+            <a href="{{route('promotions.show')}}" class="flex">
+                <h5 class="flex-row ml-0.5 mb-2 text-2xl font-medium text-white">{{ $promotion->name }}</h5>           
+                <svg class="flex h-7 w-8 text-white ml-20" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="arrow"
+                    role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path
+                        fill="currentColor"d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z" />
+                </svg>
+            </a>
+            <div class="flex">
+                <a href="{{route('promotions.show')}}" class="flex-row text-white mr-4 cursor-pointer">Ver</a>
+                <span class="mx-4 text-white">|</span>
+                <a href="{{route('editPromotion.edit', ['promotion' => $promotion->id])}}" class=" flex-row text-white mr-4 cursor-pointer">Editar</a>
+                <span class="mx-4 text-white">|</span>
+                <form action="{{route('deletePromotion.destroy', $promotion)}}" method="POST">
 
                     @csrf
                     @method('delete')
-                    <button type="submit">Eliminar</button>
 
-                </form>
-                <br>
-            </li>
-        @endforeach
-
-    </ul>
-
-    <h3 class="font-medium text-2xl  ml-10 ">Lista de bootcamps</h3>
-
-    <div class="max-w-sm p-6 mx-5 border ml-10 mt-4 border-gray-200 rounded-lg shadow bg-[#111827]">
-        <h5 class="flex-row ml-0.5 mb-2 text-2xl font-medium text-white">
-            FemCoders Norte</h5>
-        <a href="#">
-            <svg class="flex h-7 w-8 text-white" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="arrow"
-                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path
-                    fill="currentColor"d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z" />
-            </svg>
-        </a>
-
-        <input type="submit" value="Ver"class="flex-row text-white mr-4 cursor-pointer"></input>
-        <input type="submit" value="Editar"class=" flex-row text-white mr-4 cursor-pointer"></input>
-        <input type="submit" value="Eliminar"class="flex-row text-orange-600 cursor-pointer"></input>
-
-    </div>
-
-    <button type="button"
-        class="text-white w-80 justify-around text-base my-10  ml-10 bg-[#111827] hover:bg-[#111827]/80 focus:ring-4 focus:outline-none focus:ring-[#111827]/50 rounded-lg text-md py-4  text-center inline-flex items-center">
-        Frontend Gijón
-        <svg class="flex h-7 ml-4 -mr-1 w-8" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="arrow"
-            role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path fill="currentColor"
-                d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z" />
-        </svg>
-    </button>
+                    <button class="flex-row text-orange-600 cursor-pointer" type="submit">Eliminar</button>
+                </form>  
+            </div>          
+        </div>
+    @endforeach
 @endsection
