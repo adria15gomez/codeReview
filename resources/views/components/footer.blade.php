9a9454164f7faa4a016de1f491be658a55f0da26
Sara Álvarez <saralvzlpz@gmail.com>
2023-04-19 12:42:32 +0200

    fixed footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-black text-white p-5 mt-10 bottom-0 w-full">
-    <p class="text-center">©Copyright {{ now()->year }} Factoría F5</p>
+<footer class="bg-black text-white p-5 mt-10 text-center bottom-0 w-full">
+    <p>© Copyright {{ now()->year }} Factoría F5</p>
   </footer>
   
