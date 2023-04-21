c78e4f33d8b12b1e0d4155cea51da488ebf835d6
Sara Álvarez <saralvzlpz@gmail.com>
2023-04-21 13:05:40 +0200

    fixed responsive and styles, edit bootcamp without functionality

--- resources/views/trainer/coderDetail.blade.php
+++ resources/views/trainer/coderDetail.blade.php
@@ -4,18 +4,18 @@
 <div class="flex flex-col items-center justify-center">
     <h1 class="font-regular text-3xl text-center mt-5">{{$coder->name}}</h1>
     <img src="{{ '../img/trainer/coderdetail.svg' }}" alt="bootcamp" class="h-50 xl:h-80 mt-5" loading="lazy" />
-    <h3 class="font-regular text-xl mt-5 text-center">Media global</h3>
+    <h3 class="font-regular text-xl mt-5 mb-5 text-center">Media global</h3>
     
     
-    <h3 class="font-regular text-xl mt-5 text-center">Lista de evaluaciones</h3>
+    <h3 class="font-regular text-xl mt-5 text-center mb-5">Lista de evaluaciones</h3>
     <div class="flex flex-col gap-4">
         @foreach ($evaluations as $evaluation)
-            <div class="max-w-2xl p-6 mx-5 border border-gray-200 rounded-lg shadow bg-[#04070e]">
-                <div class="flex justify-between pt-4 mb-1">
-                    <a href="{{ route('trainer.comparisonEvaluation', ['user_id' => $coder->id, 'date' => $evaluation->evaluation_date]) }}" class="flex"><span class="text-lg font-medium text-white">Evaluacion {{$evaluation->evaluation_date}}</span>
+            <div class="flex-col justify-center px-4 py-2 w-72 h-20 rounded-lg shadow bg-gray-900">
+                <div class="mb-1">
+                    <a href="{{ route('trainer.comparisonEvaluation', ['user_id' => $coder->id, 'date' => $evaluation->evaluation_date]) }}"><span class="text-lg font-medium text-white">Evaluacion {{$evaluation->evaluation_date}}</span>
                 </div>
                 <div>
-                    <a href="{{ route('trainer.comparisonEvaluation', ['user_id' => $coder->id, 'date' => $evaluation->evaluation_date]) }}" class="pt-5 text-orange-600 cursor-pointer">Ver resultados</a>
+                    <a href="{{ route('trainer.comparisonEvaluation', ['user_id' => $coder->id, 'date' => $evaluation->evaluation_date]) }}" class="font-light text-orange-600 cursor-pointer">Ver resultados</a>
                 </div>
             </div>
         @endforeach
