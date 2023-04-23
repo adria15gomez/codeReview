68b9132522ea27496d4c0f8cfd656af345ecc56b
adria15gomez <adria15gomez@gmail.com>
2023-04-23 22:20:20 +0200

    clean code

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,26 +4,34 @@
             <img src="{{ asset('img/isotype.svg') }}" class="w-10 mr-3 ml-2" alt="Factoria F5 Logo" />
         </a>
 
-        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+        <button data-collapse-toggle="navbar-default" type="button"
+            class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clip-rule="evenodd"></path>
+            </svg>
         </button>
 
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            {{-- Aqui estamos haciendo que los links pasen a ser dinámicos, por lo que despues, en cada layout le pasaremos el array de links que debe recorrer --}}
-            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+            <ul
+                class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
                 @foreach ($links as $link)
-                <li><a href="{{ $link['url'] }}" class="block py-2 pl-3 pr-4 text-gray-700 text-center rounded hover:text-orange-600 active:text-orange-600">{{ $link['title'] }}</a></li>
+                    <li><a href="{{ $link['url'] }}"
+                            class="block py-2 pl-3 pr-4 text-gray-700 text-center rounded hover:text-orange-600 active:text-orange-600">{{ $link['title'] }}</a>
+                    </li>
                 @endforeach
-                {{-- En este botón tenemos luego que incluir en el href la ruta que lleve a la vista de login  --}}
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-                    <a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" 
-                    class="flex px-4 py-2 bg-black text-white text-center rounded hover:bg-orange-600 active:text-orange-600 justify-center">
-                    Cerrar Sesión
+                    <a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();"
+                        class="flex px-4 py-2 bg-black text-white text-center rounded hover:bg-orange-600 active:text-orange-600 justify-center">
+                        Cerrar Sesión
                     </a>
                 </form>
-                
+
             </ul>
         </div>
     </div>
